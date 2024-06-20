a34584dd8fd47482e0c53b01b88eaf6bc4a0f834
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2024-06-20 20:46:21 +0100

    Update power.sh

--- ChrisTileOS/config/scripts/power.sh
+++ ChrisTileOS/config/scripts/power.sh
@@ -1,28 +1,25 @@
-#!/bin/bash
-
-# To change login sound see autostart.sh file
 active_sounds=yes
 sounds_dir=.config/sounds/
 
-chosen=$(printf "Lock\nLogout\nReboot\nPowerOff\n" | rofi -p "Power" -dmenu -i -theme-str)
+chosen=$(printf " Lock\n󰍃 Logout\n󰜉 Reboot\n⏻ PowerOff\n" | rofi -p "Power" -dmenu -i -theme-str)
 
 case "$chosen" in
-        "Lock")
-                        yes_no=$(printf "no\nyes" | rofi -p "Would You Like to $chosen" -dmenu -i -theme-str)
+        " Lock")
+                        yes_no=$(printf " no\n yes" | rofi -p "Would You Like to $chosen" -dmenu -i -theme-str)
                         case $yes_no in
-                                [no]* ) exit;;
-                                [yes]* ) 
-                                        if [[ "$active_sounds" = yes ]]; then 
-                                                mpv --no-video $sounds_dir/game-bonus-144751.mp3 && xscreensaver-command -lock 
-                                        else 
+                                [no]* ) exit;;
+                                [yes]* )
+                                        if [[ "$active_sounds" = yes ]]; then
+                                                mpv --no-video $sounds_dir/game-bonus-144751.mp3 && xscreensaver-command -lock
+                                        else
                                                 xscreensaver-command -lock
                                         fi
                         esac;;
-        "Logout")
-                        yes_no=$(printf "no\nyes" | rofi -p "Would You Like to $chosen" -dmenu -i -theme-str)
+        "󰍃 Logout")
+                        yes_no=$(printf " no\n yes" | rofi -p "Would You Like to $chosen" -dmenu -i -theme-str)
                         case $yes_no in
-                                [no]* ) exit;;
-                                [yes]* )
+                                [no]* ) exit;;
+                                [yes]* )
                                         if [[ "$active_sounds" = yes ]]; then
                                                 mpv --no-video .config/sounds/cute-level-up-3-189853.mp3 && qtile cmd-obj -o cmd -f shutdown
                                         else
@@ -30,11 +27,11 @@ case "$chosen" in
 
                                         fi
                         esac;;
-        "Reboot")
-                        yes_no=$(printf "no\nyes" | rofi -p "Would You Like to $chosen" -dmenu -i -theme-str)
+        "󰜉 Reboot")
+                        yes_no=$(printf " no\n yes" | rofi -p "Would You Like to $chosen" -dmenu -i -theme-str)
                         case $yes_no in
-                                [no]* ) exit;;
-                                [yes]* ) 
+                                [no]* ) exit;;
+                                [yes]* )
                                         if [[ "$active_sounds" = yes ]]; then
                                                 mpv --no-video .config/sounds/ambient-piano-logo-165357.mp3 && reboot
                                         else
@@ -42,17 +39,14 @@ case "$chosen" in
                                         fi
 
                         esac;;
-        "PowerOff")
-                        yes_no=$(printf "no\nyes" | rofi -p "Would You Like to $chosen" -dmenu -i -theme-str)
+        "⏻ PowerOff")
+                        yes_no=$(printf " no\n yes" | rofi -p "Would You Like to $chosen" -dmenu -i -theme-str)
                         case $yes_no in
-                                [no]* ) exit;;
-                                [yes]* ) 
+                                [no]* ) exit;;
+                                [yes]* )
                                         if [[ "$active_sounds" = yes ]]; then
                                                 mpv --no-video .config/sounds/lovelyboot1-103697.mp3 && poweroff
                                         else
                                                 poweroff
                                         fi
-                        esac;;
-esac
-
-
+      
